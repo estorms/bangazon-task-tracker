77330a493b66dbccfaa5ef74f2136d30a8513c7b
Elizabeth Sanger <libsang@gmail.com>
2017-01-16 20:09:10 -0600

    added model and db context

---
+++ src/Bangazon-Task-Tracker/Models/UserTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon_Task_Tracker.Models
+{
+    public class UserTask
+    {
+        [Key]
+        public  int UserTaskId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public enum TaskStatus { ToDo, InProgress, Complete }
+
+        public TaskStatus Status { get; set; }
+        
+        public DateTime CompletedOn { get; set; }
+    }
+}
